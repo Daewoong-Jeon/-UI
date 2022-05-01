c6b5d29e6daefca1cf57fd7639a85e6ad1e96ff8
djeon <daewoong.jeon@kt.com>
2022-05-01 16:24:43 +0900

    feat: #6 #11 add modify page and fixed card size on main page

--- front/src/components/ProgressCom.vue
+++ front/src/components/ProgressCom.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
-        <div style="display: flex">
-            <CardCom step="Step1" :process="process" :index="0"/>
-            <CardCom step="Step3" :process="process" :index="1"/>
-            <CardCom step="Step2" :process="process" :index="2"/>
-            <CardCom step="Step4" :process="process" :index="3"/>
+        <b-card-group deck>
+            <div style="width: 25%;"><CardCom step="Step1" :process="process" :index="0"/></div>
+            <div style="width: 25%;"><CardCom step="Step2" :process="process" :index="1"/></div>
+            <div style="width: 25%;"><CardCom step="Step3" :process="process" :index="2"/></div>
+            <div style="width: 25%;"><CardCom step="Step4" :process="process" :index="3"/></div>
+        </b-card-group>
+        <b-card-group deck>
+            <div style="width: 25%;"><CardCom step="Step5" :process="process" :index="4"/></div>
+            <div style="width: 25%;"><CardCom step="Step6" :process="process" :index="5"/></div>
+            <div style="width: 25%;"><CardCom step="Step7" :process="process" :index="6"/></div>
+        </b-card-group>
+            
+        <!-- <div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step1" :process="process" :index="0"/></div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step2" :process="process" :index="1"/></div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step3" :process="process" :index="2"/></div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step4" :process="process" :index="3"/></div>
         </div>
-        <div style="display: flex">
-            <CardCom step="Step5" :process="process" :index="4"/>
-            <CardCom step="Step6" :process="process" :index="5"/>
-            <CardCom step="Step7" :process="process" :index="6"/>
-        </div>
+        <div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step5" :process="process" :index="4"/></div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step6" :process="process" :index="5"/></div>
+            <div style="border: 1px solid black; float: left; width: 25%; height: 200px;"><CardCom step="Step7" :process="process" :index="6"/></div>
+        </div> -->
     </div>
 </template>
 <script>
